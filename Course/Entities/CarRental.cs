e35868d0886044de88c52ed7240ef87effc4f587
Nelio Alves <acenelio@gmail.com>
2018-05-14 11:41:58 -0300

    Initial implementation

---
+++ Course/Entities/CarRental.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Course.Entities {
+    class CarRental {
+
+        public string CarModel { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Invoice Invoice { get; set; }
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        public CarRental(string carModel, DateTime start, DateTime finish, double pricePerHour, double pricePerDay) {
+            CarModel = carModel;
+            Start = start;
+            Finish = finish;
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            Invoice = null;
+        }
+
+        public void ProcessInvoice() {
+
+            TimeSpan duration = Finish.Subtract(Start);
+
+            double basicPayment;
+            if (duration.TotalHours <= 12.0) {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax;
+            if (basicPayment <= 100.00) {
+                tax = basicPayment * 0.2;
+            }
+            else {
+                tax = basicPayment * 0.15;
+            }
+
+            Invoice = new Invoice(basicPayment, tax);
+        }
+
+        public double TotalPayment() {
+            return Invoice.BasicPayment + Invoice.Tax;
+        }
+    }
+}
---
+++ Course/Entities/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Course.Entities {
+    class Invoice {
+
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax) {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public override string ToString() {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
